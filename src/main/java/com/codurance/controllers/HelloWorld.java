e0246425480fce9a3b725f25738af7c559799917
Sandro Mancuso <sandromancuso@yahoo.com>
2014-05-26 17:06:21 +0100

    View book details.

--- src/main/java/com/codurance/controllers/HelloWorld.java
+++ src/main/java/com/codurance/controllers/HelloWorld.java
@@ -1,16 +1,15 @@
 package com.codurance.controllers;
 
 import com.codurance.infrastructure.template.jade.ViewModel;
+import spark.Response;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.codurance.infrastructure.template.jade.JadeRenderer.render;
-import static java.lang.Boolean.parseBoolean;
 import static java.lang.Double.parseDouble;
 import static java.util.Comparator.reverseOrder;
 import static spark.Spark.*;
@@ -58,6 +57,7 @@ public class HelloWorld {
 				Book book = new Book(
 					nextId,
 					request.queryParams("name"),
+					request.queryParams("description"),
 					request.queryParams("available") != null,
 					parseDouble(request.queryParams("price"))
 				);
@@ -71,6 +71,27 @@ public class HelloWorld {
 				throw new RuntimeException(e);
 			}
 		});
+
+		get("/books/:id", (request, response) -> {
+			try {
+				int id = Integer.valueOf(request.params(":id"));
+				Optional<Book> book = bookList.stream().collect(Collectors.groupingBy(Book::getId)).get(id).stream().findAny();
+				if (!book.isPresent()) {
+					return notFound(response);
+				}
+				ViewModel model = new ViewModel();
+				model.put("book", book.get());
+				return render("bookdetails.jade", model);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		});
+	}
+
+	private static String notFound(Response response) {
+		response.status(404);
+		return "Not Found";
 	}
 
 	private static int nextBookId() {
@@ -80,9 +101,9 @@ public class HelloWorld {
 	}
 
 	private static void createBooks() {
-		bookList.add(new Book(nextBookId(), "Book A", true, 10));
-		bookList.add(new Book(nextBookId(), "Book B", false, 20));
-		bookList.add(new Book(nextBookId(), "Book C", true, 30));
+		bookList.add(new Book(nextBookId(), "Book A", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu.", true, 10));
+		bookList.add(new Book(nextBookId(), "Book B", "Ut sollicitudin mi et felis laoreet tempor. Sed tincidunt, nisl.", false, 20));
+		bookList.add(new Book(nextBookId(), "Book C", "Vivamus id sem magna. Phasellus non elit vel tortor adipiscing.", true, 30));
 	}
 
 	public static class Book {
@@ -91,12 +112,14 @@ public class HelloWorld {
 		private final String name;
 		private final int id;
 		private final double price;
+		private final String description;
 
-		public Book(int id, String name, boolean available, double price) {
+		public Book(int id, String name, String description, boolean available, double price) {
 			this.id = id;
 			this.name = name;
 			this.available = available;
 			this.price = price;
+			this.description = description;
 		}
 
 		public int getId() {
@@ -105,6 +128,10 @@ public class HelloWorld {
 
 		public String getName() {
 			return this.name;
+		}
+
+		public String getDescription() {
+			return description;
 		}
 
 		public boolean isAvailable() {
--- src/main/java/com/codurance/infrastructure/template/jade/JadeRenderer.java
+++ src/main/java/com/codurance/infrastructure/template/jade/JadeRenderer.java
@@ -2,15 +2,16 @@ package com.codurance.infrastructure.template.jade;
 
 import de.neuland.jade4j.Jade4J;
 
-import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class JadeRenderer {
 
-	public static String BASE_TEMPLATE_FOLDER = "./src/main/static/public/templates/";
+	public static Path BASE_TEMPLATE_FOLDER = Paths.get("./src/main/static/public/templates/");
 
 	public static String render(String template, ViewModel viewModel) {
 		try {
-			return Jade4J.render("./src/main/static/public/templates/index.jade", viewModel);
+			return Jade4J.render(BASE_TEMPLATE_FOLDER.resolve(template).toUri().toURL(), viewModel);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
