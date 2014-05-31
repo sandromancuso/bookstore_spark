3e66a8f54d9810bd1e07ed6bde2c440c60cbf843
Sandro Mancuso <sandromancuso@yahoo.com>
2014-05-31 16:33:28 +0300

    Inital webdriver test exploration

--- src/test/java/user_journey/books/ListAllBooksSpec.java
+++ src/test/java/user_journey/books/ListAllBooksSpec.java
@@ -1,28 +1,32 @@
 package user_journey.books;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.Assert.assertNotNull;
+
 
 public class ListAllBooksSpec {
 
+	private WebDriver driver = new HtmlUnitDriver();
+
 	@Given("^there are some books available$")
 	public void there_are_some_books_available() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		throw new PendingException();
 	}
 
 	@When("^I go to the books page$")
 	public void I_go_to_the_books_page() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		throw new PendingException();
+		driver.get("http://localhost:4567");
 	}
 
 	@Then("^I should see all the books$")
 	public void I_should_see_all_the_books() throws Throwable {
-		// Express the Regexp above with the code you wish you had
-		throw new PendingException();
+	    assertNotNull(driver.findElement(By.linkText("Book A")));
+
 	}
 
 }
